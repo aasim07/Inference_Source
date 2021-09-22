e42394e890faf9169d3cc7b52fa7f4b608c382ff
Aasim Sayyad <55590434+aasim07@users.noreply.github.com>
2021-09-22 17:19:06 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -6,9 +6,9 @@ model = pickle.load(open('model.pkl','rb'))
 class_names=['27.9','31.5','20.6','1011.5','8.5']
 
 def predict(df):
-  df = df[["T", "TM", "Tm", "SLP" ,"V"]]
-  
-  predictions=model.predict(df)
+  df = df[['T', 'TM', 'Tm', 'SLP' ,'V']]
+ 
+  predictions=model.predict([['T', 'TM', 'Tm', 'SLP' ,'V']])
   
   output=[class_names[class_predict] for class_predict in predictions]
   return output
